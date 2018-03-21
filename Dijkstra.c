0bb505953e5668c2e58f8d4b6d7274022b1be56e
MrAndMrswang <chaowang93@163.com>
2018-03-20 23:47:40 -0700

    commit to ChangeMain

--- Dijkstra.c
+++ Dijkstra.c
@@ -50,41 +50,36 @@ void ShortestPath_Dijkstra(MGraph G,int v0, Patharc *P, ShortPathTable *D)
 
 int main()
 {
-	int i,j;
+	int i,j,k,w;
 	MGraph G;
-	G.numVertexes = 9;
-	G.numEdges = 16;
+	cout<<"Input NumVertexes: ";
+	cin>>G.numVertexes;
+	cout<<"Input NumEdges: ";
+	cin>>G.numEdges;
 	for(i=0;i<G.numVertexes;i++)
 		for(j=0;j<G.numVertexes;j++)
 			G.arc[i][j]=INFINITY;
-	G.arc[0][1]=1;
-	G.arc[0][2]=5;
-	G.arc[1][2]=3;
-	G.arc[1][3]=7;
-	G.arc[1][4]=5;
-	G.arc[2][4]=1;
-	G.arc[2][5]=7;
-	G.arc[3][4]=2;
-	G.arc[4][5]=3;
-	G.arc[3][6]=3;
-	G.arc[4][6]=6;
-	G.arc[4][7]=9;
-	G.arc[5][7]=5;
-	G.arc[6][7]=2;
-	G.arc[6][8]=7;
-	G.arc[7][8]=4;
 	for(i=0;i<G.numVertexes;i++)
-		for(j=0;j<G.numVertexes;j++)
-			{
-				if(G.arc[i][j]!=INFINITY)
-						G.arc[j][i]=G.arc[i][j];
-			}
+	{
+		cout<<"Please input Vertexes "<<i<<" name :";
+		cin>>G.vexs[i];
+	}
+	for(k=0;k<G.numEdges;k++)
+	{
+		cout<<"Please input i j and w"<<endl;
+		cin>>i>>j>>w;
+		G.arc[i][j]=w;
+		G.arc[j][i]=w;
+	}
 	Patharc *P=new Patharc[MAXVEX];
 	ShortPathTable *D = new ShortPathTable[MAXVEX];
 	ShortestPath_Dijkstra(G,0, P, D);
+	cout<<"\nthis is shortest table: ";
 	for(i=0;i<G.numVertexes;i++)
 	{
-		cout<<(*D)[i]<<endl;
+		
+		cout<<(*D)[i]<<" ";
 	}
+	cout<<endl;
 	return 1;
 }
